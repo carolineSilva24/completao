bac3534e291d0840b226d54b8cae8a65f35141f1
carolineSilva24 <102956881+carolineSilva24@users.noreply.github.com>
2023-11-27 16:07:42 -0300

    Update script.ts

--- src/script.ts
+++ src/script.ts
@@ -7,7 +7,7 @@ const prisma = new PrismaClient()
 
 async function main() {
  //Insira as queries neste formato:
-    // const user1 = await prisma.usuario.create({
+    // const usuario1 = await prisma.usuario.create({
     //     data: {
     //         nome: "Marley",
     //         email: "marnemarne@marne.com.uvv"
@@ -31,52 +31,20 @@ async function main() {
     //         valor: 12.2
     //     }
     // })
- /* await prisma.usuario.create({
-    data: {
-        nome: 'otavio fulano',
-        email: 'fulano@gmail.com'
+ 
+ /*    const usuario2 = await prisma.usuario.create({
+           data: {
+               nome: 'otavio fulano',
+               email: 'fulano@gmail.com'
     }
-  })
+       })
 
-  await prisma.usuario.create({
-    data:{
-       nome: 'Caroline Andrade',
-       email: 'carolineAndrade@gmail.com'
-    }
-  })*/
-
- /* await prisma.lance.create({
-    data:{
-      compradorid: 'otavio fulano',
-      leilaoid: '1',
-      valor: 3000
-    }
-  })
-
-  await prisma.leilao.create({
-    data:{
-      produto: 'computador',
-      preco: 3000,
-      datalimite: '31 December 2023 23:59:59 GMT',
-      donoid: 'otavio fulano',
-      lista_de_lances: '2'
+       const usuario3 = await prisma.usuario.create({
+           data:{
+               nome: 'Caroline Andrade',
+               email: 'carolineAndrade@gmail.com'
     }
   })*/
-  
-    /*console.log(await UsuarioService.listUsuario())
-    console.log(await UsuarioService.createUsuario())
-    console.log(await UsuarioService.updateUsuario())
-    console.log(await UsuarioService.deleteUsuario('15aafa44-0ce8-413f-8b8e-7dee49849f59'))
-
-    console.log(await LeilaoService.listLeilao())
-    console.log(await LeilaoService.createLeilao())
-    console.log(await LeilaoService.updateLeilao())
-    console.log(await LeilaoService.fecharLeilao(''))
-
-    console.log(await LanceService.listLance())
-    console.log(await LanceService.createLance())
-    console.log(await LanceService.updateLance())
-    console.log(await LanceService.deleteLance(''))*/
 }
 
 main()
